d16f46595d357acb3a4d0c88b765f63d339a8c0c
GBELSHKKRS <engr.farman319@gmail.com>
2025-03-09 15:41:45 +0500

    Add files via upload

---
+++ java.java
@@ -0,0 +1,16 @@
+package java.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hellow world green");
+         System.out.println("hellow world green");
+          System.out.println("hellow world green");
+           System.out.println("hellow world green");
+            System.out.println("hellow world green");
+             System.out.println("hellow world green");
+              System.out.println("hellow world green");
+               System.out.println("hellow world green");
+    }
+}
+  
